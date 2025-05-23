ea41f780fe1e8b794fe0cdbc52f9da2e1a72b4f8
Tim Riedel <timriedel00@gmail.com>
2025-05-23 18:46:58 +0200

    feat: add tokenization and pickling of tokenized datasets

--- src/detector.py
+++ src/detector.py
@@ -1,3 +1,4 @@
+import re
 from abc import ABC, abstractmethod
 
 import pandas as pd
@@ -12,14 +13,32 @@ class Detector(ABC):
         self.labels = pd.DataFrame(ErrorType.NO_ERROR.value, index=self.dataset.index, columns=self.dataset.columns)
 
 
-    @abstractmethod
-    def detect(self):
+    def detect(self, use_tokenized_dataset: bool = False):
         """
         Detects the errors in the dataset.
         This method should be implemented by subclasses.
+        Parameters:
+            :use_tokenized_dataset: If True, uses an already tokenized dataset for detection and skips the tokenization step.
         """
-        raise NotImplementedError("Subclasses should implement this method.")
+        if use_tokenized_dataset:
+            self.tokenized_dataset = self.io_handler.load_pickled_dataset()
+        else:
+            print("Tokenizing dataset...")
+            self.tokenized_dataset = self._tokenize()
+            self.io_handler.save_pickled_dataset(self.tokenized_dataset)
+
+        print(self.tokenized_dataset.head())
 
 
     def export(self):
         self.io_handler.export_labels(self.labels)
+
+
+    def _tokenize(self):
+        tokenized_dataset = pd.DataFrame(list(), index=self.dataset.index, columns=self.dataset.columns)
+        for column in self.dataset.columns:
+            tokenized_dataset[column] = self.dataset[column].apply(
+                lambda cell: [word for word in re.split(r'\W', cell) if word] # remove empty strings
+            )
+        return tokenized_dataset
+
