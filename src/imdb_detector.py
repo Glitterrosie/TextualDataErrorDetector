5854433121f31065d26c70cc68cc8180b5d8fc79
Rosmarie Debski <64656859+Glitterrosie@users.noreply.github.com>
2025-05-26 00:23:15 +0200

    feat: add imdb detection

--- src/imdb_detector.py
+++ src/imdb_detector.py
@@ -10,3 +10,8 @@ class IMDBDetector(Detector):
         print(f"Number of cells: {self.dataset.size}, Number of rows: {self.dataset.shape[0]}")
 
         super().detect(use_tokenized_dataset) 
+
+        number_columns = ["cast_id", "cast_person_id", "cast_movie_id", "cast_nr_order", "cast_role_id", "person_id", "person_info_type_id", "title_id", "kind_id", "production_year", "episode_of_id", "season_nr", "episode_nr", "series_years"]
+        self.check_for_ocr(number_columns) #2.07%
+        self.check_for_typo() #3.31%
+        self.check_for_misspellings()
