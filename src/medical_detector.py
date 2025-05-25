b7657b166579a88cbdf2907b2531a4d9c9324525
Rosmarie Debski <64656859+Glitterrosie@users.noreply.github.com>
2025-05-26 00:23:30 +0200

    feat: add medical detection

--- src/medical_detector.py
+++ src/medical_detector.py
@@ -10,3 +10,8 @@ class MedicalDetector(Detector):
         print(f"Number of cells: {self.dataset.size}, Number of rows: {self.dataset.shape[0]}")
 
         super().detect(use_tokenized_dataset)
+
+        number_columns = ["encounter_id", "patient_nbr", "age", "weight", "admission_type_id", "discharge_disposition_id", "admission_source_id", "time_in_hospital", "num_lab_procedures", "num_procedures", "num_medications", "number_outpatient", "number_emergency", "number_inpatient", "diag_1"]
+        self.check_for_ocr(number_columns)
+        self.check_for_typo()
+        self.check_for_misspellings()
