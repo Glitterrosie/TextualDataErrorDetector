8d8bab82eaaac1eca0de63790e82b02ad76b7906
Rosmarie Debski <64656859+Glitterrosie@users.noreply.github.com>
2025-05-27 16:35:37 +0200

    refactor: uncomment imdb

--- src/main.py
+++ src/main.py
@@ -6,9 +6,9 @@ from weather_detector import WeatherDetector
 def main():
     use_tokenized_dataset = False # setting this to false will tokenize the dataset and save it as a pickle file
 
-    # imdb_detector = IMDBDetector("../datasets/imdb_subset1_group1_w_errors.csv")
-    # imdb_detector.detect(use_tokenized_dataset)
-    # imdb_detector.export()
+    imdb_detector = IMDBDetector("../datasets/imdb_subset1_group1_w_errors.csv")
+    imdb_detector.detect(use_tokenized_dataset)
+    imdb_detector.export()
 
     weather_detector = WeatherDetector("../datasets/weather_subset1_group1_w_errors.csv")
     weather_detector.detect(use_tokenized_dataset)
