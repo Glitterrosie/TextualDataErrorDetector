65bf5f1f30a7b472f35d67a8df6f7fdece504ef3
Rosmarie Debski <64656859+Glitterrosie@users.noreply.github.com>
2025-06-04 09:39:31 +0200

    refactor: introduce regex global variable

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import re
 
+REGEX = r'\W+'
+
 class Tokenizer():
     def tokenize_dataset(self, dataset):
         tokenized_dataset = pd.DataFrame(list(), index=dataset.index, columns=dataset.columns)
         for column in dataset.columns:
             tokenized_dataset[column] = dataset[column].apply(
-                lambda cell: [word for word in re.split(r'\W', cell) if word] # remove empty strings
+                lambda cell: [word for word in re.split(REGEX, cell) if word] # remove empty strings
             )
         return tokenized_dataset
     
     def tokenize_column(self, one_column_data: pd.Series) -> pd.Series:
         return one_column_data.apply(
-            lambda cell: [word for word in re.split(r'\W+', str(cell)) if word]
+            lambda cell: [word for word in re.split(REGEX, str(cell)) if word]
         )
     
     def tokenize_cell(self, cell_value: str) -> str:
-        return re.split(r'\W+', str(cell_value))
+        return re.split(REGEX, str(cell_value))
