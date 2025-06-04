a0737dad977b26be7641f38f78a85d148a1a2787
Tim Riedel <timriedel00@gmail.com>
2025-06-04 10:59:38 +0200

    refactor: remove empty strings from tokenization

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,21 +1,24 @@
-import pandas as pd
 import re
+
+import pandas as pd
 
 REGEX = r'\W+'
 
 class Tokenizer():
-    def tokenize_dataset(self, dataset):
+    def tokenize_dataset(self, dataset: pd.DataFrame) -> pd.DataFrame:
         tokenized_dataset = pd.DataFrame(list(), index=dataset.index, columns=dataset.columns)
         for column in dataset.columns:
             tokenized_dataset[column] = dataset[column].apply(
-                lambda cell: [word for word in re.split(REGEX, cell) if word] # remove empty strings
+                lambda cell: self.tokenize_cell(cell)
             )
         return tokenized_dataset
     
     def tokenize_column(self, one_column_data: pd.Series) -> pd.Series:
         return one_column_data.apply(
-            lambda cell: [word for word in re.split(REGEX, str(cell)) if word]
+            lambda cell: self.tokenize_cell(cell)
         )
     
-    def tokenize_cell(self, cell_value: str) -> str:
-        return re.split(REGEX, str(cell_value))
+    def tokenize_cell(self, cell_value: str) -> list:
+        all_tokens = re.split(REGEX, str(cell_value))
+        non_empty_tokens = [token for token in all_tokens if token]
+        return non_empty_tokens
