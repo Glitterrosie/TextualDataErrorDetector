5f655b2ec3838610830a9cd73fdbf5aca6cc1b36
Tim Riedel <timriedel00@gmail.com>
2025-06-11 18:43:16 +0200

    refactor: return value from is_not_a_number_function

--- src/utils/generic_label_utils.py
+++ src/utils/generic_label_utils.py
@@ -13,13 +13,27 @@ def is_not_a_number(value: str) -> bool:
     """
     Check if a string can be converted to an integer or float.
     """
-    try:
-        float(value)
-        return 0
-    except ValueError:
-        return 1
+    if not is_a_number(value):
+        return value
+    return 0
+
+def is_not_a_number_in_range(value: str, min_value: float, max_value: float) -> bool:
+    if not is_a_number(value):
+        return value
+    if float(value) < min_value or float(value) > max_value:
+        return value
+    return 0
 
 def is_a_number(value: str) -> bool:
+    """
+    Do not use this function in generic labeling, because it returns False or True, instead of 0 or the value itself.
+    """
+    if (value[-1] == "."): # numbers like 8743. are likely OCRs but would be converted to 8743 by the float conversion
+        return False
+
+    if (len(value) > 1 and value[0] == "0" and value[1] != "."): # numbers like 08.1 are also likely a mistake
+        return False
+
     try:
         float(value)
         return True
